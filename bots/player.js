8d0b68685fec5743a925bc44dfed7eb401522c20
Ryan Graffy <ryan.graffy@gmail.com>
2018-04-29 09:00:05 -0400

    Give bot writers some boilerplate to help them get started

--- bots/player.js
+++ bots/player.js
@@ -1,32 +1,34 @@
-class Player
-{
-	constructor()
-	{
+import { PlayerActions } from "../game/simple-code-clash";
+
+// TODO: Should we import these and use them?
+// import { PlayOutcome, RoundResult } from '../framework/framework.js';
+
+class Player {
+
+	constructor() {
+    // TODO: Maybe we should keep track of turn number?  Perhaps previous round results?
+	};
+	
+	onMatchStart() {
+	};
+	
+	onRoundStart() {
 		
 	};
 	
-	onMatchStart()
-	{
-		
-	};
-	
-	onRoundStart()
-	{
-		
-	};
-	
-	playTurn()
-	{
-		//no implementation, could return INVALID
-		return ' ';
-		
+	playTurn() {
+
+	  // TODO: figure out what we want to return
+    //return PlayerActions.TRAIN_ATTACKER;
+    //return PlayerActions.BUILD_DEFENSE;
+
+		return PlayerActions.ATTACK;
 	}
 	
-	onRoundEnd()
-	{
+	onRoundEnd() {
 		
 	};
 	
 }
 
-export {Player};
+export { Player };
